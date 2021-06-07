8cca9cf69ef0d030aee9dc2527222e43d658e5a3
Aakriti Yadav <aakriti.yadav@bigsteptech.com>
2021-06-08 00:57:49 +0530

    Validator added

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -42,7 +42,7 @@ export class SearchBarComponent implements OnInit {
     this.addForm = new FormGroup({
       'name': new FormControl(null, [Validators.required, Validators.maxLength(20)]),
       'address': new FormControl(null),
-      'mobile': new FormControl(null, [Validators.maxLength(10), Validators.minLength(10)])
+      'mobile': new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.minLength(10)])
     })
 
   }
