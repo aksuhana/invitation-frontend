f8081e35146df32d8a1c7204c693d26b830ef918
Sahil <sahilsubnani49@gmail.com>
2021-06-11 12:47:20 +0530

    changing app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,44 +6,41 @@ import { HindiNameService } from './HindiName.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  appsub: Subscription
-  latestUserName: string=""
-  currentUserName: string= ""
+  appsub: Subscription;
+  latestUserName: string = '';
+  currentUserName: string = '';
   title = 'invitation-frontend';
-  language: string = "english";
+  language: string = 'english';
   lang: any;
-  constructor(private hindiService: HindiNameService,
-              private infoHandler: InfoHandlerService) {
-
-  }
-  ngOnInit(){
+  constructor(
+    private hindiService: HindiNameService,
+    private infoHandler: InfoHandlerService
+  ) {}
+  ngOnInit() {
     this.appsub = this.infoHandler.latest.subscribe(
-      (latestUser)=>this.latestUserName=latestUser
-    )
+      (latestUser) => (this.latestUserName = latestUser)
+    );
   }
-  ngDoCheck(){
-    if(this.latestUserName!==this.currentUserName)
-    {
-      this.currentUserName=this.latestUserName
-      console.log(this.currentUserName)
+  ngDoCheck() {
+    if (this.latestUserName !== this.currentUserName) {
+      this.currentUserName = this.latestUserName;
+      console.log(this.currentUserName);
     }
   }
   toggle() {
-    if (this.language === "english") {
-      this.language = "hindi";
-      this.hindiService.LangSelected("hindi");
+    if (this.language === 'english') {
+      this.language = 'hindi';
+      this.hindiService.LangSelected('hindi');
+    } else if (this.language === 'hindi') {
+      this.language = 'english';
+      this.hindiService.LangSelected('english');
     }
-    else
-      if (this.language === "hindi") {
-        this.language = "english";
-        this.hindiService.LangSelected("english");
-      }
   }
-  onActivate(){
-    console.log("event triggered!!!!!");
-    console.log()
+  onActivate() {
+    console.log('event triggered!!!!!');
+    console.log();
   }
 }
