c8cebbe2494197efcf4aed5eda432fa8eb215a5e
Sahil <sahilsubnani49@gmail.com>
2021-06-11 12:49:53 +0530

    changing services

--- src/app/HindiName.service.ts
+++ src/app/HindiName.service.ts
@@ -2,15 +2,13 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HindiNameService {
-
   private hindiSource = new BehaviorSubject('english');
   hindiMessage = this.hindiSource.asObservable();
-  constructor() { }
+  constructor() {}
   LangSelected(message: string) {
-    this.hindiSource.next(message)
+    this.hindiSource.next(message);
   }
-
 }
